e037bb71b41873f4f23e1f43c47c92b63de0dd28
sudip-khanal <101724348+sudip-khanal@users.noreply.github.com>
2023-04-02 16:38:59 +0545

    initial commit

---
+++ website/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect
+
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .forms import SignUpForm
+
+
+
+# Create your views here.
+def home(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Login successfully")
+            return redirect('home')
+        else:
+            messages.success(request,"Error occure please try again")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request," You have been Loggedout.............")
+    return redirect('home')
+
+
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+
+    
